b0ade57db4a491c26e0ac2fe48e29b7f9b9811be
Abanoub Nashat <nashatabanop@gmail.com>
2026-09-19 18:18:50 +0300

    Implement toggle functionality for AddFriendForm and update Button component to accept onClick prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import FriendsList from "./components/FriendsList";
 import AddFriendForm from "./components/AddFriendForm";
 import Button from "./components/Button";
 import SplitBillForm from "./components/SplitBillForm";
 
 export default function App() {
+  //----------------------------------------------
+  // Where does the form show in our app ? in App Component then the state we need to make that form show and hide is in App Component not in the Form itself.
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+  //-----------------------------------------------
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList></FriendsList>
-        <AddFriendForm></AddFriendForm>
-        <Button>Add Friend</Button>
+        {showAddFriend && <AddFriendForm></AddFriendForm>}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
       <SplitBillForm></SplitBillForm>
     </div>
