d67804fb8dbef84c989ed91c65b96675a047e0af
Abanoub Nashat <nashatabanop@gmail.com>
2026-09-19 16:33:38 +0300

    Refactor scripts in package.json to use explicit paths; implement FriendsList and FriendItem components in App.js

--- src/App.js
+++ src/App.js
@@ -18,3 +18,46 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <FriendsList></FriendsList>
+    </div>
+  );
+}
+
+function FriendsList(params) {
+  return (
+    <div className="sidebar">
+      <ul>
+        {initialFriends.map((friend) => (
+          <FriendItem friend={friend}></FriendItem>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function FriendItem({ friend }) {
+  return (
+    <li key={friend.id}>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          {" "}
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {" "}
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
